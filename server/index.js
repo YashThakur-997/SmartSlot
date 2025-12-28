46428ba463827c47ded2016dca8c0afd53b6a3d3
Yash Thakur <yashthakur99755@gmail.com>
2025-12-28 19:50:34 +0530

    removed practice material

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ app.get('/',(req,res)=>{
     res.send('Hello World');
 })
 
-app.get('/health',(req,res)=>{
-    res.send('Server is healthy');
-})
-
 app.listen(PORT ,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
